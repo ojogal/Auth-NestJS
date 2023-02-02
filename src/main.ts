03824e56671642b7d0613e0c22dd38ba32cf24e3
ojogal <ojog.al@gmail.com>
2023-02-02 12:01:14 +0200

    User's entities class updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function server() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4001);
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  const config: ConfigService = app.get(ConfigService);
+  const port: number = config.get<number>('PORT');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(port, () => {
+    console.log(`Server works on address http://localhost:${port}`);
+  });
 }
+
 server();
